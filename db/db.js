91a46627face80ff8875ae1c098933c4d3163377
maheshdila <maheshdilshan716@gmail.com>
2024-07-01 12:12:56 +0530

    db file updated

--- db/db.js
+++ db/db.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-require("dotenv").config();
-
-const uri = process.env.MONGO_DB_URL;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(uri);
-    console.log("MongoDB connection established");
-  } catch (error) {
-    console.error("MongoDB connection failed", error);
+    const mongoUri = process.env.MONGO_URI;
+    if (!mongoUri) {
+      throw new Error("MONGO_URI is not defined");
+    }
+
+    console.log(`Connecting to MongoDB at: ${mongoUri}`); // Debugging line
+    await mongoose.connect(mongoUri);
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
     process.exit(1);
   }
 };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+require("dotenv").config();  // Load environment variables
 
 const connectDB = require("./db/db");
 const errorHandler = require("./middlewares/errorhandler");
@@ -12,23 +12,19 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-
-
-//routes
+// Routes
 app.use("/api/users", userRoutes);
-
 app.use("/api/products", (req, res) => {
   return res.status(200).json({
     message: "This is new feature change, a new route for products samin",
   });
 });
 
-//error handler
+// Error handler
 app.use(errorHandler);
 
-//listen to the server
+// Listen to the server and connect to the database
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
-  //connect to the database
   connectDB();
 });
